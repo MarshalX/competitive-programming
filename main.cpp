1f52a0441fae637afd2bcac429a10314e36e2777
Il'ya (Marshal) <ilya@marshal.by>
2021-11-05 17:19:01 +0100

    12

--- main.cpp
+++ main.cpp
@@ -1,46 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <cmath>
+#include <sstream>
 
 using namespace std;
 
 
 class Solution {
 public:
-  map<char, int> m = map<char, int>{
-      {'I', 1},
-      {'V', 5},
-      {'X', 10},
-      {'L', 50},
-      {'C', 100},
-      {'D', 500},
-      {'M', 1000},
+  map<int, char> m = map<int, char>{
+      {1000, 'M'},
+      {500, 'D'},
+      {100, 'C'},
+      {50, 'L'},
+      {10, 'X'},
+      {5, 'V'},
+      {1, 'I'},
   };
 
-  int romanToInt(string s) {
-    int number = 0;
-    for (int i = 0; i < s.length(); ++i) {
-      char next = i == s.length() - 1 ? '-' : s[i + 1];
+  map<int, string> exc = map<int, string>{
+    {4, "IV"},
+    {40, "XL"},
+    {400, "CD"},
 
-      if (
-          (s[i] == 'I' && (next == 'V' || next == 'X')) |
-          (s[i] == 'X' && (next == 'L' || next == 'C')) |
-          (s[i] == 'C' && (next == 'D' || next == 'M'))
-          ) {
-        number -= m[s[i]];
+    {9, "IX"},
+    {90, "XC"},
+    {900, "CM"},
+  };
+
+  string intToRoman(int num) {
+    string s = to_string(num);
+    vector<string> resParts = {"", "", "", ""};
+
+    for (int i = s.length() - 1; i >= 0; --i) {
+      int n = s[i] - '0';
+      int rank = s.length() - i - 1;
+      int e = pow(10, rank);
+
+      int nn = n * e;
+      if (exc.find(nn) != exc.end()) {
+        resParts[i] = exc[nn];
       } else {
-        number += m[s[i]];
-      }
+        string tmp = "";
+        int da = nn;
+        for (auto p = m.rbegin(); p != m.rend(); ++p) {
+          if (da >= p->first) {
+            int k = da / p->first;
+            da -= p->first * k;
+            for (int j = 0; j < k; ++j) {
+              tmp += p->second;
+            }
+          }
+        }
+
+        resParts[i] = tmp;
+      };
     }
 
-    return number;
+    stringstream ss;
+    ss << resParts[0] << resParts[1] << resParts[2] << resParts[3];
+    return ss.str();
   }
 };
 
 int main() {
-  cout << (new Solution())->romanToInt("LVIII") << endl;
-  cout << (new Solution())->romanToInt("IV") << endl;
-  cout << (new Solution())->romanToInt("MCMXCIV") << endl;
+  cout << (new Solution())->intToRoman(1) << endl; // II
+  cout << (new Solution())->intToRoman(2) << endl; // II
+  cout << (new Solution())->intToRoman(3) << endl; // III
+  cout << (new Solution())->intToRoman(58) << endl; // LVIII
+  cout << (new Solution())->intToRoman(4) << endl; // IV
+  cout << (new Solution())->intToRoman(1994) << endl; // MCMXCIV
+  cout << (new Solution())->intToRoman(2994) << endl; // MMCMXCIV
+  cout << (new Solution())->intToRoman(3594) << endl; // MMMDXCIV
 
   return 0;
 }
