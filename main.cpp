56a491e30805886aaae084efa3ddcfc3182e7b41
Il'ya (Marshal) <ilya@marshal.by>
2021-11-05 11:27:17 +0100

    234

--- main.cpp
+++ main.cpp
@@ -2,20 +2,35 @@
 
 using namespace std;
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-  bool isPalindrome(int x) {
-    string s = to_string(x);
-    int r = s.length() - 1;
-    for (int l = 0; l < s.length(); ++l) {
-      if (l == r) {
-        break;
-      }
+  bool isPalindrome(ListNode *head) {
+    auto ih = *head;
+    int len = 1;
+    while (head->next != nullptr) {
+      len++;
+      head = head->next;
+    }
 
-      if (s[l] != s[r]) {
-        return false;
-      }
+    auto a = new int[len];
+    a[0] = ih.val;
+    for (int i = 1; i < len; ++i) {
+      ih = *ih.next;
+      a[i] = ih.val;
+    }
 
+    int r = len - 1;
+    for (int l = 0; l < len; ++l) {
+      if (l == r) {break;}
+      if (a[l] != a[r]) {return false;}
       r--;
     }
 
@@ -24,10 +39,8 @@ public:
 };
 
 int main() {
-  int x;
-  while (1) {
-    cin >> x;
-    cout << ((new Solution())->isPalindrome(x) ? "true" : "false") << endl;
-  }
+  auto* x = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+
+  cout << ((new Solution())->isPalindrome(x) ? "true" : "false") << endl;
   return 0;
 }
