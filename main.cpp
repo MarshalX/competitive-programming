07da37422ff5a7ecd47379fd0ed8ba7ef856d50d
Il'ya (Marshal) <ilya@marshal.by>
2021-11-06 17:22:24 +0100

    37

--- main.cpp
+++ main.cpp
@@ -13,31 +13,56 @@ public:
   const int subSize = 3;
   vector<vector<int>> b = vector<vector<int>>();
 
-  bool isValidCell(int x, int y) {
-    for (int i = 0; i < size; ++i) {
-      if (b[x][i] == b[x][y] && y != i) {
-        return false;
-      }
+  vector<int> getAvailable(int x, int y) {
+    vector<int> avail = vector<int>(10);
+    fill(avail.begin(), avail.end(), 0);
+    avail[0] = 1;
 
-      if (b[i][y] == b[x][y] && x != i) {
-        return false;
-      }
+    for (int i = 0; i < size; ++i) {
+      avail[b[x][i]] = 1;
+      avail[b[i][y]] = 1;
     }
 
     int xStart = x / subSize;
     int yStart = y / subSize;
     for (int j = xStart * 3; j < xStart * subSize + subSize; ++j) {
       for (int k = yStart * 3; k < yStart * subSize + subSize; ++k) {
-        if (b[j][k] == b[x][y] && (j != x && k != y)) {
-          return false;
-        }
+        avail[b[j][k]] = 1;
       }
     }
 
-    return true;
+    return avail;
   }
 
-  bool isValidSudoku(vector<vector<char>>& board) {
+  bool solver(int x, int y) {
+    if ((x == size - 1) && (y == size)) {
+      return true;
+    }
+
+    if (y == size) {
+      y = 0;
+      x += 1;
+    }
+
+    if (b[x][y] == 0) {
+      auto avail = getAvailable(x, y);
+      for (int i = 1; i < 10 ; ++i) {
+        if (avail[i] == 0) {
+          b[x][y] = i;
+          if (solver(x, y + 1)) {
+            return true;
+          }
+        }
+      }
+
+      b[x][y] = 0;
+      return false;
+    }
+
+    return solver(x, y + 1);
+  }
+
+  void solveSudoku(vector<vector<char>>& board) {
     for (int i = 0; i < board.size(); ++i) {
       b.emplace_back();
       for (int j = 0; j < board[i].size(); ++j) {
@@ -49,15 +74,13 @@ public:
       }
     }
 
-    for (int i = 0; i < b.size(); ++i) {
-      for (int j = 0; j < b[i].size(); ++j) {
-        if (b[i][j] != 0 && !isValidCell(i, j)) {
-          return false;
-        }
+    solver(0, 0);
+
+    for (int i = 0; i < board.size(); ++i) {
+      for (int j = 0; j < board[i].size(); ++j) {
+        board[i][j] = b[i][j] + '0';
       }
     }
-
-    return true;
   }
 };
 
