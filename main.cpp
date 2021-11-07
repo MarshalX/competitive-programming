66a868e7dec0d9f8ced9721f8a473869bb6f1081
Il'ya (Marshal) <ilya@marshal.by>
2021-11-07 18:12:09 +0100

    227 input parsing

--- main.cpp
+++ main.cpp
@@ -10,25 +10,30 @@ using namespace std;
 
 class Solution {
 public:
-  vector<string> stringMatching(vector<string>& words) {
-    vector<string> res;
-    set<string> u;
-    for (int i = 0; i < words.size(); ++i) {
-      for (int j = 0; j < words.size(); ++j) {
-        if (i == j) { continue; }
-        if (words[i].find(words[j]) != -1) {
-          if (u.find(words[j]) == u.end()) {
-            u.insert(words[j]);
-            res.push_back(words[j]);
-          }
-        }
+  int calculate(string s) {
+    string operand = "";
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == ' ') { continue; }
+      if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+        cout << operand << " " << s[i] << " ";
+        operand = "";
+      } else {
+        operand += s[i];
       }
     }
-    return res;
+
+    if (!operand.empty()) {
+      cout << operand << endl;
+    }
+
+    // TODO polish notation
+
+    return 0;
   }
 };
 
 int main() {
-
+  cout << Solution().calculate("3 + 5 / 2 ") << endl;
+  cout << (int)'0'<< " " << (int)'9'<< endl;
   return 0;
 }
