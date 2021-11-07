d16283350f99f335cb3345a7d142cdcc30bc5225
Il'ya (Marshal) <ilya@marshal.by>
2021-11-07 19:11:54 +0100

    21

--- main.cpp
+++ main.cpp
@@ -8,27 +8,63 @@
 using namespace std;
 
 
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+
 class Solution {
 public:
-  vector<string> stringMatching(vector<string>& words) {
-    vector<string> res;
-    set<string> u;
-    for (int i = 0; i < words.size(); ++i) {
-      for (int j = 0; j < words.size(); ++j) {
-        if (i == j) { continue; }
-        if (words[i].find(words[j]) != -1) {
-          if (u.find(words[j]) == u.end()) {
-            u.insert(words[j]);
-            res.push_back(words[j]);
-          }
-        }
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr) {
+      return l2;
+    }
+    if (l2 == nullptr) {
+      return l1;
+    }
+
+    ListNode* res = nullptr;
+    ListNode* start = nullptr;
+    while (l1 != nullptr && l2 != nullptr) {
+      auto* tmp = new ListNode(l1->val <= l2->val ? l1->val : l2-> val);
+
+      if (res == nullptr) {
+        start = tmp;
+      } else {
+        res->next = tmp;
+      }
+      res = tmp;
+
+      if (l1->val <= l2->val) {
+        l1 = l1->next;
+      } else {
+        l2 = l2->next;
       }
     }
-    return res;
+
+    if (l1 != nullptr) {
+      res->next = l1;
+    }
+    if (l2 != nullptr) {
+      res->next = l2;
+    }
+
+    return start;
   }
 };
 
 int main() {
+  auto* l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+  auto* l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+  auto* res = Solution().mergeTwoLists(l1, l2);
+  while (res != nullptr) {
+    cout << res->val << " ";
+    res = res->next;
+  }
 
   return 0;
 }
