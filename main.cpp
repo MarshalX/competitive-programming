ad926b10973681411e76ad8d8037edf88f621ca5
Il'ya (Marshal) <ilya@marshal.by>
2021-11-07 19:38:38 +0100

    20

--- main.cpp
+++ main.cpp
@@ -4,67 +4,43 @@
 #include <cmath>
 #include <sstream>
 #include <set>
+#include <stack>
 
 using namespace std;
 
 
-struct ListNode {
-   int val;
-   ListNode *next;
-   ListNode() : val(0), next(nullptr) {}
-   ListNode(int x) : val(x), next(nullptr) {}
-   ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
-
-
 class Solution {
 public:
-  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    if (l1 == nullptr) {
-      return l2;
-    }
-    if (l2 == nullptr) {
-      return l1;
-    }
+  map<char, char> m = {
+      {'(', ')'},
+      {'[', ']'},
+      {'{', '}'},
+  };
 
-    ListNode* res = nullptr;
-    ListNode* start = nullptr;
-    while (l1 != nullptr && l2 != nullptr) {
-      auto* tmp = new ListNode(l1->val <= l2->val ? l1->val : l2-> val);
-
-      if (res == nullptr) {
-        start = tmp;
+  bool isValid(string s) {
+    stack<char> st = stack<char>();
+    for (int i = 0; i < s.length(); ++i) {
+      if (m.find(s[i]) != m.end()) {
+        st.push(s[i]);
       } else {
-        res->next = tmp;
-      }
-      res = tmp;
+        if (st.empty()) {
+          return false;
+        }
 
-      if (l1->val <= l2->val) {
-        l1 = l1->next;
-      } else {
-        l2 = l2->next;
+        if (m[st.top()] != s[i]) {
+          return false;
+        }
+        st.pop();
       }
     }
 
-    if (l1 != nullptr) {
-      res->next = l1;
-    }
-    if (l2 != nullptr) {
-      res->next = l2;
-    }
-
-    return start;
+    return st.empty();
   }
 };
 
 int main() {
-  auto* l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-  auto* l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-  auto* res = Solution().mergeTwoLists(l1, l2);
-  while (res != nullptr) {
-    cout << res->val << " ";
-    res = res->next;
-  }
+  cout << Solution().isValid("()") << endl;
+  cout << Solution().isValid("]") << endl;
 
   return 0;
 }
