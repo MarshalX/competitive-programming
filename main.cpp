6b2a5be0d03e7b7219645f8841f24beba80f036c
Il'ya (Marshal) <ilya@marshal.by>
2021-11-11 21:40:53 +0100

    28. KMP alg

--- main.cpp
+++ main.cpp
@@ -16,16 +16,40 @@ public:
       return 0;
     }
 
-    int startIndex;
-    for (int i = 0; i < haystack.length(); ++i) {
-      startIndex = i;
-      for (int j = 0; j < needle.length(); ++j) {
-         if (i + j > haystack.length() - 1) { break; }
+    auto t = vector<int>(needle.length());
 
-        if (haystack[i + j] != needle[j]) {
-          break;
-        } else if (j == needle.length() - 1) {
-          return startIndex;
+    int pos = 1;
+    int cnd = 0;
+
+    t[0] = -1;
+    while (pos < needle.length()) {
+      if (needle[pos] == needle[cnd]) {
+        t[pos] = t[cnd];
+      } else {
+        t[pos] = cnd;
+        while (cnd >= 0 && needle[pos] != needle[cnd]) {
+          cnd = t[cnd];
+        }
+      }
+
+      pos++;
+      cnd++;
+    }
+
+    int j = 0;
+    int k = 0;
+    while (j < haystack.length()) {
+      if (needle[k] == haystack[j]) {
+        j++;
+        k++;
+        if (k == needle.length()) {
+          return j - k;
+        }
+      } else {
+        k = t[k];
+        if (k < 0) {
+          j++;
+          k++;
         }
       }
     }
@@ -36,6 +60,9 @@ public:
 
 
 int main() {
+  cout << Solution().strStr("aaaaa", "bba") << endl;
+  cout << Solution().strStr("", "a") << endl;
+  cout << Solution().strStr("a", "a") << endl;
   cout << Solution().strStr("ooo", "ooooooooo") << endl;
   cout << Solution().strStr("hello", "ll") << endl;
 
