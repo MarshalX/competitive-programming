b86ddc27fc22820f2fc1d3e4008ddc2c2471e454
Il'ya (Marshal) <ilya@marshal.by>
2021-11-23 21:57:03 +0100

    53. TL (prefix sum)

--- main.cpp
+++ main.cpp
@@ -11,59 +11,43 @@ using namespace std;
 
 class Solution {
 public:
-  int strStr(string haystack, string needle) {
-    if (needle.empty()) {
-      return 0;
+  int maxSubArray(vector<int>& nums) {
+    auto p = vector<int>(nums.size());
+    p[0] = nums[0];
+
+    int res = p[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      p[i] = p[i - 1] + nums[i];
     }
 
-    auto p = vector<int>(needle.length());
+    for (auto &n: p) {
+      cout << n << " ";
+    }
+    cout << endl;
 
-    int i = 1;
-    int j = 0;
+    for (int i = 0; i < p.size(); ++i) {
+      for (int j = i + 1; j < p.size(); ++j) {
+        int sum = p[j];
+        res = max(res, sum);
 
-    p[0] = 0;
-    while (i < needle.length()) {
-      if (needle[i] == needle[j]) {
-        p[i] = j + 1;
-        i++; j++;
-      } else {
-        if (j == 0) {
-          p[i] = 0;
-          i++;
-        } else {
-          j = p[j - 1];
-        }
+        sum = p[j] - p[i];
+        res = max(res, sum);
+
       }
     }
 
-    i = 0;
-    j = 0;
-    while (i < haystack.length()) {
-      if (haystack[i] == needle[j]) {
-        i++; j++;
-        if (j == needle.length()) {
-          return i - j;
-        }
-      } else {
-        if (j > 0) {
-          j = p[j - 1];
-        } else {
-          i++;
-        }
-      }
-    }
-
-    return -1;
+    return res;
   }
 };
 
 
 int main() {
-  cout << Solution().strStr("aaaaa", "bba") << endl;
-  cout << Solution().strStr("", "a") << endl;
-  cout << Solution().strStr("a", "a") << endl;
-  cout << Solution().strStr("ooo", "ooooooooo") << endl;
-  cout << Solution().strStr("hello", "ll") << endl;
+  auto v = vector<int>{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  auto v1 = vector<int>{5, 4, -1, 7, 8};
+  auto v3 = vector<int>{-2, 1};
+  cout << Solution().maxSubArray(v) << endl;
+  cout << Solution().maxSubArray(v1) << endl;
+  cout << Solution().maxSubArray(v3) << endl;
 
   return 0;
 }
