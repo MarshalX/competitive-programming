033279b2c414b36506602ad3172e06374f17018e
Il'ya (Marshal) <ilya@marshal.by>
2021-11-24 21:18:41 +0100

    697. Runtime: 934 ms, faster than 5.05%

--- main.cpp
+++ main.cpp
@@ -11,14 +11,45 @@ using namespace std;
 
 class Solution {
 public:
-  int maxSubArray(vector<int>& nums) {
-    int res = -10e5;
-    int tmpSum = 0;
+  int findShortestSubArray(vector<int>& nums) {
+    auto m = map<int, int>();
+    int res = 50000;
 
-    for (int num : nums) {
-      tmpSum += num;
-      tmpSum = max(tmpSum, num);
-      res = max(res, tmpSum);
+    for (int n:nums) {
+      if (m.find(n) == m.end()) {
+        m[n] = 1;
+      } else {
+        m[n] += 1;
+      }
+    }
+
+    std::set<std::pair<int, int>> s;
+    for (auto const &i : m) {
+      s.emplace(i.second, i.first);
+    }
+
+    int max = -1;
+    for (auto i = s.rbegin(); i != s.rend(); ++i) {
+      if (max == -1) {
+        max = i->first;
+      }
+
+      if (i->first < max) {
+        break;
+      }
+
+      int start = -1;
+      int stop = -1;
+      for (int j = 0; j < nums.size(); ++j) {
+        if (start == -1 && nums[j] == i->second) {
+          start = j;
+        }
+        if (nums[j] == i->second) {
+          stop = j;
+        }
+      }
+
+      res = min(res, stop - start + 1);
     }
 
     return res;
@@ -27,14 +58,12 @@ public:
 
 
 int main() {
-  auto v = vector<int>{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-  auto v1 = vector<int>{5, 4, -1, 7, 8};
-  auto v3 = vector<int>{-2, 1};
-  auto v4 = vector<int>{-1};
-  cout << Solution().maxSubArray(v) << endl;
-  cout << Solution().maxSubArray(v1) << endl;
-  cout << Solution().maxSubArray(v3) << endl;
-  cout << Solution().maxSubArray(v4) << endl;
+  auto v = vector<int>{1, 2, 2, 3, 1}; // 2
+  auto v1 = vector<int>{1, 2, 2, 3, 1, 4, 2}; // 6
+  auto v2 = vector<int>{1}; // 1
+  cout << Solution().findShortestSubArray(v) << endl;
+  cout << Solution().findShortestSubArray(v1) << endl;
+  cout << Solution().findShortestSubArray(v2) << endl;
 
   return 0;
 }
