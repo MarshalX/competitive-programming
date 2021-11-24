7ab975a6852e5a156f724c54e15bbf8316dcc0d7
Il'ya (Marshal) <ilya@marshal.by>
2021-11-24 10:07:22 +0100

    53

--- main.cpp
+++ main.cpp
@@ -12,28 +12,13 @@ using namespace std;
 class Solution {
 public:
   int maxSubArray(vector<int>& nums) {
-    auto p = vector<int>(nums.size());
-    p[0] = nums[0];
+    int res = -10e5;
+    int tmpSum = 0;
 
-    int res = p[0];
-    for (int i = 1; i < nums.size(); ++i) {
-      p[i] = p[i - 1] + nums[i];
-    }
-
-    for (auto &n: p) {
-      cout << n << " ";
-    }
-    cout << endl;
-
-    for (int i = 0; i < p.size(); ++i) {
-      for (int j = i + 1; j < p.size(); ++j) {
-        int sum = p[j];
-        res = max(res, sum);
-
-        sum = p[j] - p[i];
-        res = max(res, sum);
-
-      }
+    for (int num : nums) {
+      tmpSum += num;
+      tmpSum = max(tmpSum, num);
+      res = max(res, tmpSum);
     }
 
     return res;
@@ -45,9 +30,11 @@ int main() {
   auto v = vector<int>{-2, 1, -3, 4, -1, 2, 1, -5, 4};
   auto v1 = vector<int>{5, 4, -1, 7, 8};
   auto v3 = vector<int>{-2, 1};
+  auto v4 = vector<int>{-1};
   cout << Solution().maxSubArray(v) << endl;
   cout << Solution().maxSubArray(v1) << endl;
   cout << Solution().maxSubArray(v3) << endl;
+  cout << Solution().maxSubArray(v4) << endl;
 
   return 0;
 }
