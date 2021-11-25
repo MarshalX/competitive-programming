ae8580d99bd6b52b4e7da177fbdd31c69f7919c7
Il'ya (Marshal) <ilya@marshal.by>
2021-11-25 20:50:33 +0100

    1337

--- main.cpp
+++ main.cpp
@@ -11,42 +11,36 @@ using namespace std;
 
 class Solution {
 public:
-  int findShortestSubArray(vector<int>& nums) {
-    map<int, int> m;
-    map<int, vector<int>> pos;
-    int res = INT_MAX;
+  vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+    map<int, int> powers;
 
-    int maxDegree = 0;
-    for (int & num : nums) {
-      m[num] += 1;
+    int tPower;
+    for (int i = 0; i < mat.size(); ++i) {
+      tPower = 0;
+      for (int j = 0; j < mat[i].size(); ++j) {
+        if (mat[i][j] == 1) {
+          tPower++;
+        } else {
+          powers[i] = tPower;
+          break;
+        }
 
-      maxDegree = max(maxDegree, m[num]);
-    }
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (m[nums[i]] == maxDegree && pos.find(nums[i]) == pos.end()) {
-        pos[nums[i]].emplace_back(i);
-        pos[nums[i]].push_back(-1);
-      }
-    }
-
-    for (int i = nums.size() - 1; i >= 0; --i) {
-      if (m[nums[i]] == maxDegree && pos[nums[i]][1] == -1) {
-        pos[nums[i]][1] = i;
+        if (j == mat[i].size() - 1) {
+          powers[i] = tPower;
+        }
       }
     }
 
     set<pair<int, int>> s;
-    for (auto const &i : m) {
-      s.emplace(i.second, i.first);
+    for (const auto kv: powers) {
+      s.emplace(kv.second, kv.first);
     }
 
-    for (auto i = s.rbegin(); i != s.rend(); ++i) {
-      if (i->first < maxDegree) {
-        break;
-      }
-
-      res = min(res, pos[i->second][1] - pos[i->second][0] + 1);
+    vector<int> res;
+    for (const auto & i : s) {
+      if (k == 0) break;
+      k--;
+      res.push_back(i.second);
     }
 
     return res;
@@ -55,12 +49,20 @@ public:
 
 
 int main() {
-  auto v = vector<int>{1, 2, 2, 3, 1}; // 2
-  auto v1 = vector<int>{1, 2, 2, 3, 1, 4, 2}; // 6
-  auto v2 = vector<int>{1}; // 1
-  cout << Solution().findShortestSubArray(v) << endl;
-  cout << Solution().findShortestSubArray(v1) << endl;
-  cout << Solution().findShortestSubArray(v2) << endl;
+  auto v = vector<vector<int>>{{1, 1, 0, 0, 0},
+                               {1, 1, 1, 1, 0},
+                               {1, 0, 0, 0, 0},
+                               {1, 1, 0, 0, 0},
+                               {1, 1, 1, 1, 1}}; // [2, 0, 3]
+  auto v1 = vector<vector<int>>{{1, 0},
+                                {1, 0},
+                                {1, 0},
+                                {1, 1}};
+  auto r = Solution().kWeakestRows(v, 3);
+//  auto r = Solution().kWeakestRows(v1, 4);
+  for (int n:r) {
+    cout << n << " ";
+  }
 
   return 0;
 }
