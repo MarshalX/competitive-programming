1e366a63f674c9ce533e2541fae6d559008a79de
Il'ya (Marshal) <ilya@marshal.by>
2021-11-25 19:10:38 +0100

    697. Cleanup

--- main.cpp
+++ main.cpp
@@ -11,18 +11,42 @@ using namespace std;
 
 class Solution {
 public:
-  int maxProfit(vector<int>& prices) {
-    int l = 0;
-    int r = 1;
+  int findShortestSubArray(vector<int>& nums) {
+    map<int, int> m;
+    map<int, vector<int>> pos;
+    int res = INT_MAX;
 
-    int res = 0;
-    while (l < prices.size() && r < prices.size()) {
-      if (prices[l] > prices[r]) {
-        l = r;
-      } else {
-        res = max(res, prices[r] - prices[l]);
-        r++;
+    int maxDegree = 0;
+    for (int & num : nums) {
+      m[num] += 1;
+
+      maxDegree = max(maxDegree, m[num]);
+    }
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (m[nums[i]] == maxDegree && pos.find(nums[i]) == pos.end()) {
+        pos[nums[i]].emplace_back(i);
+        pos[nums[i]].push_back(-1);
       }
+    }
+
+    for (int i = nums.size() - 1; i >= 0; --i) {
+      if (m[nums[i]] == maxDegree && pos[nums[i]][1] == -1) {
+        pos[nums[i]][1] = i;
+      }
+    }
+
+    set<pair<int, int>> s;
+    for (auto const &i : m) {
+      s.emplace(i.second, i.first);
+    }
+
+    for (auto i = s.rbegin(); i != s.rend(); ++i) {
+      if (i->first < maxDegree) {
+        break;
+      }
+
+      res = min(res, pos[i->second][1] - pos[i->second][0] + 1);
     }
 
     return res;
@@ -31,14 +55,12 @@ public:
 
 
 int main() {
-  auto v = vector<int>{7, 1, 5, 3, 6, 4}; // 5
-  auto v1 = vector<int>{7, 6, 4, 3, 1}; // 0
-  auto v2 = vector<int>{1}; // 0
-  auto v3 = vector<int>{2, 1, 4}; // 3
-  cout << Solution().maxProfit(v) << endl;
-  cout << Solution().maxProfit(v1) << endl;
-  cout << Solution().maxProfit(v2) << endl;
-  cout << Solution().maxProfit(v3) << endl;
+  auto v = vector<int>{1, 2, 2, 3, 1}; // 2
+  auto v1 = vector<int>{1, 2, 2, 3, 1, 4, 2}; // 6
+  auto v2 = vector<int>{1}; // 1
+  cout << Solution().findShortestSubArray(v) << endl;
+  cout << Solution().findShortestSubArray(v1) << endl;
+  cout << Solution().findShortestSubArray(v2) << endl;
 
   return 0;
 }
