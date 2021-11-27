a507ae4e8671bd6b49eaaa2c9a5e9c8d69b1f5d3
Il'ya (Marshal) <ilya@marshal.by>
2021-11-27 23:41:39 +0100

    724

--- main.cpp
+++ main.cpp
@@ -10,33 +10,39 @@
 using namespace std;
 
 
-class NumArray {
+class Solution {
 public:
-  vector<int> p;
+  int pivotIndex(vector<int>& nums) {
+    vector<int> p;
 
-  NumArray(vector<int>& nums) {
     p.push_back(nums[0]);
     for (int i = 1; i < nums.size(); ++i) {
       p.push_back(p[i - 1] + nums[i]);
     }
-  }
 
-  int sumRange(int left, int right) {
-    if (left > 0) {
-      return p[right] - p[left - 1];
-    } else {
-      return p[right];
+    int last = nums.size() - 1;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (i == 0) {
+        if (p[last] - nums[i] == 0) {
+          return 0;
+        }
+      } else if (p[i - 1] == p[last] - p[i]) {
+        return i;
+      }
     }
+
+    return -1;
   }
 };
 
 
 int main() {
-  auto v = vector<int>{-2, 0, 3, -5, 2, -1};
-  auto n = NumArray(v);
-  cout << n.sumRange(0, 2) << endl;
-  cout << n.sumRange(2, 5) << endl;
-  cout << n.sumRange(0, 5) << endl;
+  auto v = vector<int>{1, 7, 3, 6, 5, 6};
+  auto v1 = vector<int>{2, 1, -1};
+  auto v3 = vector<int>{2, 1, -1};
+  cout << Solution().pivotIndex(v) << endl;
+  cout << Solution().pivotIndex(v1) << endl;
+  cout << Solution().pivotIndex(v3) << endl;
 
   return 0;
 }
