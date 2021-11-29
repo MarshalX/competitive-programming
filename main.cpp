c2076106cf17e37a8fb288a6541cc7029e6e5c2f
Il'ya (Marshal) <ilya@marshal.by>
2021-11-29 20:20:38 +0100

    190

--- main.cpp
+++ main.cpp
@@ -12,12 +12,10 @@ using namespace std;
 
 class Solution {
 public:
-  int hammingWeight(uint32_t n) {
+  uint32_t reverseBits(uint32_t n) {
     int res = 0;
     for (int i = 0; i < 32; ++i) {
-      if ((n & (1 << i))) {
-        res++;
-      }
+      if (n & (1 << i)) res = res ^ (1 << (31 - i));
     }
     return res;
   }
