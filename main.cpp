fc874758789e1703255246c1dc9cbd63187a43a2
Il'ya (Marshal) <ilya@marshal.by>
2021-11-29 19:50:23 +0100

    231

--- main.cpp
+++ main.cpp
@@ -12,44 +12,25 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> sumZero(int n) {
-    vector<int> res;
-
-    int half = n / 2;
-    int tmp = 0;
-
-    int i = 1;
-    for (int j = 0; j < half; ++j) {
-      if (j == half - 1) {
-        int t = abs(tmp - n);
-        if (t < i) t = i;
-        res.push_back(t);
-        res.push_back(-t);
-      } else {
-        res.push_back(i);
-        res.push_back(-i);
-        tmp += i;
-        i++;
-      }
+  bool isPowerOfTwo(int n) {
+    for (int i = 0; i < 32; ++i) {
+      if (i == 31 && n < 0) {
+        if (n == (1L << i) - 1) return true;
+      } else if (n == (1 << i)) return true;
     }
 
-    if (n % 2 != 0) {
-      res.push_back(0);
-    }
-
-    return res;
+    return false;
   }
 };
 
 
 int main() {
-  auto res = Solution().sumZero(5);
-//  auto res = Solution().sumZero(2);
-//  auto res = Solution().sumZero(8);
-
-  for (const int i : res) {
-    cout << i << " ";
-  }
+  cout << Solution().isPowerOfTwo(1) << endl;
+  cout << Solution().isPowerOfTwo(16) << endl;
+  cout << Solution().isPowerOfTwo(-16) << endl;
+  cout << Solution().isPowerOfTwo(3) << endl;
+  cout << Solution().isPowerOfTwo(-2147483648) << endl; // 0
+  cout << Solution().isPowerOfTwo(2147483647) << endl; // 0
 
   return 0;
 }
