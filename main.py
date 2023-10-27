368a0aa26f431fbc96cc4ba9b2be9278fbc2c8d6
Ilya (Marshal) <ilya@marshal.dev>
2023-10-27 20:17:40 +0200

    125. Set

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from typing import List
+import string
 
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        res = nums[0]
-        for i in range(1, len(nums)):
-            res ^= nums[i]
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
 
-        return res
+        chars = list(s)
+        valid_chars = set(string.ascii_lowercase + string.digits)
+
+        s1 = ''
+        s2 = ''
+        for char in chars:
+            if char in valid_chars:
+                s1 += char
+                s2 = char + s2
+
+        return s1 == s2
 
 
 if __name__ == '__main__':
     cases = [
-        [2, 2, 1],
-        [4, 1, 2, 1, 2],
-        [1],
+        "A man, a plan, a canal: Panama",
+        "race a car",
+        " ",
     ]
 
     for case in cases:
-        print(Solution().singleNumber(case))
+        print(Solution().isPalindrome(case))
