e4c65d0d7c4feca7e02f1f791d55bb576f01b867
Ilya (Marshal) <ilya@marshal.dev>
2023-10-28 12:19:26 +0200

    509. dp

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 class Solution:
-    memo = {}
-
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        dp = {0: 0, 1: 1}
 
-        if n in self.memo:
-            return self.memo[n]
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
-        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
-        return self.memo[n]
+        return dp[n]
 
 
 if __name__ == '__main__':
