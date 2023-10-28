945c609e90027f49805946001f243628838f3717
Ilya (Marshal) <ilya@marshal.dev>
2023-10-28 22:48:04 +0200

    35

--- main.py
+++ main.py
@@ -2,34 +2,32 @@ from typing import List
 
 
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {0: 0}
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
 
-        for i in range(1, amount + 1):
-            for coin in coins:
-                subproblem = i - coin
-                if subproblem < 0 or coin > i:
-                    continue
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-                cur_min = dp.get(i, float('INF'))
-                prev_min = dp.get(subproblem, float('INF'))
-                dp[i] = min(cur_min, prev_min + 1)
+        if right == mid:
+            return mid + 1
 
-        res = dp.get(amount)
-        if res == float('INF') or res is None:
-            return -1
-
-        return res
+        return mid
 
 
 if __name__ == '__main__':
     cases = [
-        ([5], 15),
-        ([1, 2, 5], 11),
-        ([2], 3),
-        ([1], 0),
-        ([470, 35, 120, 81, 121], 9825),
+        ([1, 3, 6], 5),
+        ([1, 3, 5, 6], 5),
+        ([1, 3, 5, 6], 2),
+        ([1, 3, 5, 6], 7),
     ]
 
     for case in cases:
-        print(Solution().coinChange(case[0], case[1]))
+        print(Solution().searchInsert(case[0], case[1]))
