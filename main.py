6938f82babd1fb5a02c7891fc2b0aa0b84398a62
Ilya (Marshal) <ilya@marshal.dev>
2023-10-28 12:17:14 +0200

    509. Memo + recursive

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        chars = list(s.lower())
-        chars_count = len(chars)
+    memo = {}
 
-        p1 = 0
-        p2 = chars_count - 1
-        while p1 < chars_count and p2 > 0 and p1 < p2:
-            s1, s2 = chars[p1], chars[p2]
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
 
-            if not s1.isalnum():
-                p1 += 1
-                continue
+        if n in self.memo:
+            return self.memo[n]
 
-            if not s2.isalnum():
-                p2 -= 1
-                continue
-
-            if s1 != s2:
-                return False
-
-            p1 += 1
-            p2 -= 1
-
-        return True
+        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
 
 
 if __name__ == '__main__':
     cases = [
-        "A man, a plan, a canal: Panama",
-        "race a car",
-        " ",
-        "aa",
+        2,
+        3,
+        4,
+        10,
+        11,
+        60
     ]
 
     for case in cases:
-        print(Solution().isPalindrome(case))
+        print(Solution().fib(case))
