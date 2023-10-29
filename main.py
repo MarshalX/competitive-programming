0d9c396fd0a52885e25081fc642fafecb6b28eaf
Ilya (Marshal) <ilya@marshal.dev>
2023-10-29 22:16:43 +0100

    58

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
+    def lengthOfLastWord(self, s: str) -> int:
         res = 0
 
-        left = 0
-        right = n - 1
-        while left <= right:
-            mid = (left + right) // 2
+        n = len(s) - 1
+        while n >= 0:
+            if s[n].isspace() and res != 0:
+                return res
+            if not s[n].isspace():
+                res += 1
 
-            if isBadVersion(mid + 1):
-                res = mid + 1
-
-            if not isBadVersion(mid + 1):
-                left = mid + 1
-            else:
-                right = mid - 1
+            n -= 1
 
         return res
 
 
 if __name__ == '__main__':
     cases = [
-        5, 6, 10
+        "a",
+        "Hello World",
+        "   fly me   to   the moon  ",
     ]
 
     for case in cases:
-        print(Solution().firstBadVersion(case))
+        print(Solution().lengthOfLastWord(case))
