ab7eef4e5b40654d9265a666040a701fa7d76294
Ilya (Marshal) <ilya@marshal.dev>
2023-10-29 20:07:14 +0100

    278

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-from typing import List
-
-
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def firstBadVersion(self, n: int) -> int:
+        res = 0
+
         left = 0
-        right = len(nums) - 1
+        right = n - 1
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
 
-            if nums[mid] < target:
+            if isBadVersion(mid + 1):
+                res = mid + 1
+
+            if not isBadVersion(mid + 1):
                 left = mid + 1
             else:
                 right = mid - 1
 
-        if right == mid:
-            return mid + 1
-
-        return mid
+        return res
 
 
 if __name__ == '__main__':
     cases = [
-        ([1, 3, 6], 5),
-        ([1, 3, 5, 6], 5),
-        ([1, 3, 5, 6], 2),
-        ([1, 3, 5, 6], 7),
+        5, 6, 10
     ]
 
     for case in cases:
-        print(Solution().searchInsert(case[0], case[1]))
+        print(Solution().firstBadVersion(case))
