11ac6cdebded1a86775d32465a947f8f69fb5d15
Ilya (Marshal) <ilya@marshal.dev>
2023-11-04 13:54:38 +0100

    83

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f'Node(v={self.val}, next={self.next})'
+
+
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
 
-        def _(r):
-            if r is None:
-                return []
+        root = head
+        while root is not None:
+            cur = root
+            while cur.next and root.val == cur.next.val:
+                cur = cur.next
 
-            _(r.left)
-            _(r.right)
-            res.append(r.val)
+            root.next = cur.next
+            root = root.next
 
-        _(root)
-
-        return res
+        return head
 
 
 if __name__ == '__main__':
-    pass
+    c1 = ListNode(1, ListNode(1, ListNode(2)))
+    print(Solution().deleteDuplicates(c1))
+
+    c2 = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3)))))
+    print(Solution().deleteDuplicates(c2))
