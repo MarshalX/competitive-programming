1051c709ed8ea56c42cbdbae387dc21b07948c4a
Ilya (Marshal) <ilya@marshal.dev>
2023-11-04 14:06:36 +0100

    104

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-    def __repr__(self):
-        return f'Node(v={self.val}, next={self.next})'
-
-
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return head
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def _(r, d) -> int:
+            if r is None:
+                return d
 
-        root = head
-        while root is not None:
-            cur = root
-            while cur.next and root.val == cur.next.val:
-                cur = cur.next
+            l = _(r.left, d + 1)
+            r = _(r.right, d + 1)
 
-            root.next = cur.next
-            root = root.next
+            return max(l, r)
 
-        return head
+        return _(root, 0)
 
 
 if __name__ == '__main__':
-    c1 = ListNode(1, ListNode(1, ListNode(2)))
-    print(Solution().deleteDuplicates(c1))
-
-    c2 = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3)))))
-    print(Solution().deleteDuplicates(c2))
+    print(Solution().maxDepth(None))
