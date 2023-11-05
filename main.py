b46d46536bf5b277a9c454b04ea6eca64d6025d9
Ilya (Marshal) <ilya@marshal.dev>
2023-11-05 15:12:56 +0100

    62

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        dp = {0: 1, 1: 2}
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = {}
+        for y in range(m):
+            dp[(n - 1, y)] = 1
+        for x in range(n):
+            dp[(x, m - 1)] = 1
 
-        for i in range(2, n):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        for x in reversed(range(n - 1)):
+            for y in reversed(range(m - 1)):
+                dp[(x, y)] = dp[(x + 1, y)] + dp[x, y + 1]
 
-        return dp[n - 1]
+        return dp[(0, 0)]
 
 
 if __name__ == '__main__':
-    print(Solution().climbStairs(1))
-    print(Solution().climbStairs(2))
-    print(Solution().climbStairs(3))
+    print(Solution().uniquePaths(3, 7))
+    print(Solution().uniquePaths(3, 2))
