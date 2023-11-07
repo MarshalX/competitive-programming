c6a5b13e82a1c7b234718e58a44d4a9cc69aa622
Ilya (Marshal) <ilya@marshal.dev>
2023-11-07 19:33:21 +0100

    100. DFS

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-import queue
 from typing import Optional
 
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        qe = queue.Queue()
-
-        qe.put(p)
-        qe.put(q)
-        while not qe.empty():
-            n1 = qe.get()
-            n2 = qe.get()
-
-            if n1 is None or n2 is None:
-                if n1 != n2:
-                    return False
-                continue
-
-            if n1.val != n2.val:
-                return False
-
-            qe.put(n1.left)
-            qe.put(n1.right)
-            qe.put(n2.left)
-            qe.put(n2.right)
-
-        return True
+        if p is None or q is None:
+            return p == q
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 
 if __name__ == '__main__':
