c814faae35d4dd89ab0a081f5b4c7bf2872b4ef9
Ilya (Marshal) <ilya@marshal.dev>
2023-11-10 20:12:44 +0100

    94. Iteratively

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-from typing import Optional
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f'Node({self.val=}, {self.left=}, {self.right=})'
 
 
 class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if root is None:
-            return None
-        if root.val == val:
-            return root
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
 
-        if res := (self.searchBST(root.left, val) or self.searchBST(root.right, val)):
+        if root is None:
             return res
 
-        return None
+        stack = [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if visited:
+                res.append(node.val)
+            else:
+                if node.left:
+                    stack.append((node.left, False))
+
+                stack.append((node, True))
+
+                if node.right:
+                    stack.append((node.right, False))
+
+        return res[::-1]
 
 
 if __name__ == '__main__':
-    pass
+    t = TreeNode(0, TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(4, TreeNode(5)))
+
+    #             0
+    #     1               4
+    # 2       3       5
+
+    # 2 1 3 0 5 4
+
+    print(Solution().inorderTraversal(t))
