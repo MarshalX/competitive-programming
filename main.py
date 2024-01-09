c205aa7b1502e7f6e4952f56a05728c5f0e544a0
Ilya (Marshal) <ilya@marshal.dev>
2024-01-09 19:14:27 +0100

    169

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from typing import List
 
 
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums)):
-            res = res ^ nums[i] ^ i
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
 
-        return res ^ i + 1
+        count = max_count = 1
+        prev = res = nums[0]
+        for n in range(1, len(nums)):
+            if nums[n] != prev:
+                count = 1
+                prev = nums[n]
+                continue
+
+            count += 1
+            if max_count < count:
+                max_count = count
+                res = nums[n]
+
+        return res
 
 
 if __name__ == '__main__':
-    print(Solution().missingNumber([3, 0, 1]))
-    print(Solution().missingNumber([0, 1]))
-    print(Solution().missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+    print(Solution().majorityElement([1]))
+    print(Solution().majorityElement([3, 2, 3]))
+    print(Solution().majorityElement([2, 2, 1, 1, 1, 2, 2]))
+    print(Solution().majorityElement([3, 3, 4]))
