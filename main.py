78d75432a3e2453033771e2d8993d424b33bdd29
Ilya (Marshal) <ilya@marshal.dev>
2024-03-06 21:57:29 +0100

    88

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ from typing import List
 
 
 class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
-        dm = {v: i for i, v in enumerate(order)}
-        wo = [[dm[c] for c in w] for w in words]
-        wa = sorted(wo)
-        return wo == wa
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        res = []
+
+        i1 = i2 = 0
+        while i1 != m:
+            if i2 >= n:
+                res.append(nums1[i1])
+                i1 += 1
+            elif nums1[i1] < nums2[i2]:
+                res.append(nums1[i1])
+                i1 += 1
+            elif nums1[i1] > nums2[i2]:
+                res.append(nums2[i2])
+                i2 += 1
+            else:
+                res.append(nums2[i2])
+                res.append(nums2[i2])
+                i1 += 1
+                i2 += 1
+
+        while i2 != n:
+            res.append(nums2[i2])
+            i2 += 1
+
+        for i in range(m + n):
+            nums1[i] = res[i]
 
 
 if __name__ == '__main__':
-    print(Solution().isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"))
-    print(Solution().isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz"))
-    print(Solution().isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz"))
-    print(Solution().isAlienSorted(["kuvp", "q"], "ngxlkthsjuoqcpavbfdermiywz"))
+    a = [1, 2, 3, 0, 0, 0]
+    Solution().merge(a, 3, [2, 5, 6], 3)
+    assert [1, 2, 2, 3, 5, 6] == a
