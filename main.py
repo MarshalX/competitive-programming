7884125e38ef4235f719dd998fd908e7d57fce09
Ilya (Marshal) <ilya@marshal.dev>
2024-04-06 23:56:31 +0200

    905

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from typing import List
 
 
 class Solution:
-    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        return [[i ^ 1 for i in reversed(r)] for r in image]
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        odd, even = [], []
+        for num in nums:
+            odd.append(num) if num & 1 == 1 else even.append(num)
+        return even + odd
 
 
 if __name__ == '__main__':
-    a = Solution().flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]])
-    assert [[1, 0, 0], [0, 1, 0], [1, 1, 1]] == a
-    a = Solution().flipAndInvertImage([[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]])
-    assert [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]] == a
+    a = Solution().sortArrayByParity([3, 1, 2, 4])
+    assert [2, 4, 3, 1] == a
+    a = Solution().sortArrayByParity([0])
+    assert [0] == a
