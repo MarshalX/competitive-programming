f1732b753f05bc52dcd7ec4e08298aa21ae126a6
Ilya (Marshal) <ilya@marshal.dev>
2024-04-06 23:49:22 +0200

    832

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-class ParkingSystem:
-    def __init__(self, big: int, medium: int, small: int):
-        self._slots = {1: 0, 2: 0, 3: 0}
-        self._slots_limits = {1: big, 2: medium, 3: small}
+from typing import List
 
-    def addCar(self, carType: int) -> bool:
-        if self._slots[carType] == self._slots_limits[carType]:
-            return False
 
-        self._slots[carType] += 1
-        return True
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        return [[i ^ 1 for i in reversed(r)] for r in image]
 
 
 if __name__ == '__main__':
-    a = ParkingSystem(1, 1, 0)
-    assert True is a.addCar(1)
-    assert True is a.addCar(2)
-    assert False is a.addCar(3)
-    assert False is a.addCar(1)
+    a = Solution().flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]])
+    assert [[1, 0, 0], [0, 1, 0], [1, 1, 1]] == a
+    a = Solution().flipAndInvertImage([[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]])
+    assert [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]] == a
