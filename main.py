3bd85806031346a68edf910e36272746f8a5fb1e
Ilya (Marshal) <ilya@marshal.dev>
2024-04-08 17:34:23 +0200

    922

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from typing import List
 
 
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        last = 0
-        for i in range(len(nums)):
-            if nums[i] & 1 != 1:
-                tmp = nums[last]
-                nums[last] = nums[i]
-                nums[i] = tmp
-                last += 1
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        odd = [i for i in nums if i & 1 == 1]
+        even = [i for i in nums if i & 1 != 1]
 
-        return nums
+        result = []
+        for i in range(len(odd)):
+            result.append(even[i])
+            result.append(odd[i])
+
+        return result
 
 
 if __name__ == '__main__':
-    a = Solution().sortArrayByParity([3, 1, 2, 4])
-    assert [2, 4, 3, 1] == a
-    a = Solution().sortArrayByParity([0])
-    assert [0] == a
+    a = Solution().sortArrayByParityII([3, 4])
+    assert [4, 3] == a
+    a = Solution().sortArrayByParityII([4, 1, 2, 1])
+    print(a)
+    a = Solution().sortArrayByParityII([2, 3])
+    assert [2, 3] == a
+    a = Solution().sortArrayByParityII([2, 2, 2, 2, 1, 1, 1, 1])
+    print(a)
+    a = Solution().sortArrayByParityII([2, 2, 2, 2, 2, 1, 1, 1, 1, 1])
+    print(a)
