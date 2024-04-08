46d5f9a33d406cb8e2308f065dc8e997b0f72847
Ilya (Marshal) <ilya@marshal.dev>
2024-04-08 23:04:05 +0200

    922. In place

--- main.py
+++ main.py
@@ -3,25 +3,38 @@ from typing import List
 
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        odd = [i for i in nums if i & 1 == 1]
-        even = [i for i in nums if i & 1 != 1]
+        p1, p2 = 0, 0
+        for i in range(len(nums)):
+            while p1 < len(nums) and nums[p1] & 1 != 1:
+                p1 += 1
+            while p2 < len(nums) and nums[p2] & 1 == 1:
+                p2 += 1
 
-        result = []
-        for i in range(len(odd)):
-            result.append(even[i])
-            result.append(odd[i])
+            if i & 1 == 1 and nums[i] & 1 != 1:
+                nums[i], nums[p1] = nums[p1], nums[i]
+            elif i & 1 != 1 and nums[i] & 1 == 1:
+                nums[i], nums[p2] = nums[p2], nums[i]
 
-        return result
+            if p1 < i:
+                p1 = i + 1
+            if p2 < i:
+                p2 = i + 1
+
+        return nums
 
 
 if __name__ == '__main__':
     a = Solution().sortArrayByParityII([3, 4])
     assert [4, 3] == a
+    a = Solution().sortArrayByParityII([4, 1, 1, 4])
+    assert [4, 1, 4, 1] == a
     a = Solution().sortArrayByParityII([4, 1, 2, 1])
-    print(a)
+    assert [4, 1, 2, 1] == a
     a = Solution().sortArrayByParityII([2, 3])
     assert [2, 3] == a
+    a = Solution().sortArrayByParityII([1, 1, 1, 1, 2, 2, 2, 2])
+    assert [2, 1, 2, 1, 2, 1, 2, 1] == a
     a = Solution().sortArrayByParityII([2, 2, 2, 2, 1, 1, 1, 1])
-    print(a)
+    assert [2, 1, 2, 1, 2, 1, 2, 1] == a
     a = Solution().sortArrayByParityII([2, 2, 2, 2, 2, 1, 1, 1, 1, 1])
-    print(a)
+    assert [2, 1, 2, 1, 2, 1, 2, 1, 2, 1] == a
