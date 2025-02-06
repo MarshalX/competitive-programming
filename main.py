c5377c3faa80e58c48e9d9f1cf68c3907a3c9126
Ilya (Marshal) <ilya@marshal.dev>
2025-02-06 22:43:52 +0100

    350. Map

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = defaultdict(lambda: 0)
+        for num in nums1:
+            d[num] += 1
+
+        res = []
+        for num in nums2:
+            if num not in d:
+                continue
+
+            if d[num] > 0:
+                res.append(num)
+                d[num] -= 1
+
+        return res
 
 
 if __name__ == '__main__':
-    a = Solution().intersection([1,2,2,1],[2,2])
-    assert [2] == a
-    b = Solution().intersection([4,9,5],[9,4,9,8,4])
+    a = Solution().intersect([1,2,2,1],[2,2])
+    assert [2, 2] == a
+    b = Solution().intersect([4,9,5],[9,4,9,8,4])
     assert [9,4] == b
