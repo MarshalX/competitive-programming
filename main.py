d6c73c9fa7528d38d762878a9736779357c49db4
Ilya (Marshal) <ilya@marshal.dev>
2025-02-07 19:38:51 +0100

    154

--- main.py
+++ main.py
@@ -3,18 +3,7 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l = 0
-        r = len(nums) - 1
-
-        while l < r:
-            m = (l + r) // 2
-
-            if nums[m] > nums[r]:
-                l = m + 1
-            else:
-                r = m
-
-        return nums[l]
+        return min(nums)
 
 
 if __name__ == '__main__':
