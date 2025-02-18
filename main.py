11e03de900dbbc8f6c782eb5a4ad6e988bf83006
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 23:17:01 +0100

    35

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
 
         while l <= r:
             m = (l + r) // 2
 
-            if nums[m] > target:
+            if target > nums[m]:
                 l = m + 1
-            elif nums[m] < target:
+            elif target < nums[m]:
                 r = m - 1
             else:
                 return m
 
-        return -1
+        return l
