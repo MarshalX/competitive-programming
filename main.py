7081be63d6f0a2ff79c7853b6fa836382a30c5b0
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 23:07:59 +0100

    704

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-     known_nums = {}
-     for i in range(len(nums)):
-         diff = target - nums[i]
-         if diff in known_nums:
-             return [i, known_nums[diff]]
-         known_nums[nums[i]] = i
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] > target:
+                l = m + 1
+            elif nums[m] < target:
+                r = m - 1
+            else:
+                return m
+
+        return -1
