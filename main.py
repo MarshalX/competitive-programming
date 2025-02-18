d1337bf8305e84eed77cff82b2688a9e810747ef
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 21:37:16 +0100

    121

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+from typing import List
+
+
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
 
-        si = 0
-        for i in t:
-            if i == s[si]:
-                si += 1
+        dp = [0] * len(prices)
+        dp[0] = prices[1] - prices[0]
+        min_price = prices[0]
+        for i in range(1, len(prices)):
+            dp[i] = max(dp[i - 1], prices[i] - min_price)
+            min_price = min(min_price, prices[i])
 
-            if si == len(s):
-                return True
-
-        return False
+        return max(0, dp[-1])
 
 
 if __name__ == '__main__':
-    assert True == Solution().isSubsequence(s = 'abc', t = 'ahbgdc')
-    assert False == Solution().isSubsequence(s = 'axc', t = 'ahbgdc')
+    assert 1 == Solution().maxProfit([1, 2])
+    assert 3 == Solution().maxProfit([1, 2, 4])
+    assert 5 == Solution().maxProfit([7,1,3,5,6,4])
+    assert 0 == Solution().maxProfit([7,6,4,3,1])
