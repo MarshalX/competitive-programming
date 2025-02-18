abac3a897f4f353d8c52c71630c2b82183f3f67f
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 22:24:52 +0100

    242

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from collections import defaultdict
 
 
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    def isAnagram(self, s: str, t: str) -> bool:
         d = defaultdict(int)
-        for letter in magazine:
+        for letter in s:
             d[letter] += 1
 
-        for letter in ransomNote:
+        for letter in t:
+            if letter not in d:
+                return False
+
             if d[letter] > 0:
                 d[letter] -= 1
             else:
+                return False
+
+        for letters_count in d.values():
+            if letters_count != 0:
                 return False
 
         return True
