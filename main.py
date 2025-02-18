5ebedd8abef1e34c8fb40c5dd60cef641ba0dab4
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 22:07:09 +0100

    228

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from typing import List
 
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs.sort()
-
-        first = strs[0]
-        last = strs[-1]
-
-        res = ''
-        for i in range(min(len(first), len(last))):
-            if first[i] != last[i]:
-                break
-            res += first[i]
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        res, range_part, range_started = [], None, False
+        for i in range(len(nums)):
+            if i < len(nums) - 1 and nums[i + 1] == nums[i] + 1:
+                if not range_started:
+                    range_part = f'{nums[i]}->'
+                    range_started = True
+            elif range_started:
+                res.append(f'{range_part}{nums[i]}')
+                range_started = False
+            else:
+                res.append(f'{nums[i]}')
 
         return res
 
-
 if __name__ == '__main__':
-    assert 'fl' == Solution().longestCommonPrefix(['flower','flow','flight'])
-    assert '' == Solution().longestCommonPrefix(['dog','racecar','car'])
+    assert [] == Solution().summaryRanges([])
+    assert ['1', '3'] == Solution().summaryRanges([1, 3])
+    assert ['1->3'] == Solution().summaryRanges([1, 2, 3])
+    assert ['0->2','4->5','7'] == Solution().summaryRanges([0,1,2,4,5,7])
+    assert ['0','2->4','6','8->9']== Solution().summaryRanges([0,2,3,4,6,8,9])
