43bce2f05da406468815fe25fa257e665f0f1aa6
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 21:50:08 +0100

    14

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            return 0
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort()
 
-        dp = [0] * len(prices)
-        dp[0] = prices[1] - prices[0]
-        min_price = prices[0]
-        for i in range(1, len(prices)):
-            dp[i] = max(dp[i - 1], prices[i] - min_price)
-            min_price = min(min_price, prices[i])
+        first = strs[0]
+        last = strs[-1]
 
-        return max(0, dp[-1])
+        res = ''
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                break
+            res += first[i]
+
+        return res
 
 
 if __name__ == '__main__':
-    assert 1 == Solution().maxProfit([1, 2])
-    assert 3 == Solution().maxProfit([1, 2, 4])
-    assert 5 == Solution().maxProfit([7,1,3,5,6,4])
-    assert 0 == Solution().maxProfit([7,6,4,3,1])
+    assert 'fl' == Solution().longestCommonPrefix(['flower','flow','flight'])
+    assert '' == Solution().longestCommonPrefix(['dog','racecar','car'])
