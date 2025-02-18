073b97a6d033c0516a5fbd32d30e505f6d900a5f
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 20:43:27 +0100

    13

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ''
-        for i in range(max(len(word1), len(word2))):
-            if i < len(word1):
-                res += word1[i]
-            if i < len(word2):
-                res += word2[i]
+    def romanToInt(self, s: str) -> int:
+        digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        rules = {
+            'I': {'V': 4, 'X': 9},
+            'X': {'L': 40, 'C': 90},
+            'C': {'D': 400, 'M': 900},
+        }
+
+        i = res = 0
+        while i != len(s):
+            if i < len(s) - 1 and s[i] in rules and s[i + 1] in rules[s[i]]:
+                res += rules[s[i]][s[i + 1]]
+                i += 1
+            else:
+                res += digits[s[i]]
+            i += 1
 
         return res
 
 
 if __name__ == '__main__':
-    assert 'apbqcrqqqq' == Solution().mergeAlternately(word1 = 'abc', word2 = 'pqrqqqq')
-    assert 'apbqcr' == Solution().mergeAlternately(word1 = 'abc', word2 = 'pqr')
-    assert 'apbqrs' == Solution().mergeAlternately(word1 = 'ab', word2 = 'pqrs')
-    assert 'apbqcd' == Solution().mergeAlternately(word1 = 'abcd', word2 = 'pq')
-    assert 'ab' == Solution().mergeAlternately(word1 = 'a', word2 = 'b')
-    assert 'ba' == Solution().mergeAlternately(word1 = 'b', word2 = 'a')
+    assert 3 == Solution().romanToInt('III')
+    assert 58 == Solution().romanToInt('LVIII')
+    assert 1994 == Solution().romanToInt('MCMXCIV')
