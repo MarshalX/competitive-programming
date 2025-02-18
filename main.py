3c4cf96a591a95a73e722cf5fbd63eeab4e876a4
Ilya (Marshal) <ilya@marshal.dev>
2025-02-18 20:21:03 +0100

    2239

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ from typing import List
 
 
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        max_x, min_y = len(grid[0]), 0
-        x, y = 0, len(grid) - 1
+    def findClosestNumber(self, nums: List[int]) -> int:
+        res_og = nums[0]
+        res = abs(0 - nums[0])
+        for n in nums:
+            d = abs(0 - n)
+            if d < res:
+                res = d
+                res_og = n
+            elif d == res:
+                res_og = max(res_og, n)
 
-        res = 0
-        while y >= min_y and x < max_x:
-            if grid[y][x] < 0:
-                res += max_x - x
-                y -= 1
-            else:
-                x += 1
-
-        return res
+        return res_og
 
 
 if __name__ == '__main__':
-    a = Solution().countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]])
-    assert a == 8
-    a = Solution().countNegatives([[3,2],[1,0]])
-    assert a == 0
+    a = Solution().findClosestNumber([-4,-2,1,4,8])
+    assert a == 1
+    a = Solution().findClosestNumber([2,-1,1])
+    assert a == 1
