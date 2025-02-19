37f3ecacb1a1f8180196d8b2d91c8c037649d35f
Ilya (Marshal) <ilya@marshal.dev>
2025-02-19 20:32:42 +0100

    876

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from typing import List
+import math
 
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        c, ll = 0, head
+        while ll:
+            c += 1
+            ll = ll.next
 
-        while l <= r:
-            m = (l + r) // 2
+        m = int(math.ceil((c + 0.5) / 2))
 
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
-            else:
-                return m
-
-        return l
+        c, ll = 0, head
+        while ll:
+            c += 1
+            if c == m:
+                return ll
+            ll = ll.next
