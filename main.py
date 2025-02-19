70caead16b96d47508f9ff3d71f55fdc17159fc6
Ilya (Marshal) <ilya@marshal.dev>
2025-02-19 21:35:32 +0100

    344

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
+from typing import List
+
+
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        res = ListNode(0, head)
-        dummy = res
-
-        for _ in range(n):
-            head = head.next
-
-        while head:
-            head = head.next
-            dummy = dummy.next
-
-        dummy.next = dummy.next.next
-
-        return res.next
+    def reverseString(self, s: List[str]) -> None:
+        last = len(s) - 1
+        for cur in range(len(s) // 2):
+            s[last], s[cur] = s[cur], s[last]
+            last -= 1
