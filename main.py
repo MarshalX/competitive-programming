5b9f980ddb6a5e89d8116456182a9f90943b7736
Ilya (Marshal) <ilya@marshal.dev>
2025-02-23 17:11:07 +0100

    112. DFS

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+from typing import Optional
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        def _(node: Optional[TreeNode], curSum: int = 0):
+            if not node:
+                return False
+
+            curSum += node.val
+            if not node.left and not node.right:
+                return targetSum == curSum
+
+            return _(node.left, curSum) or _(node.right, curSum)
+
+        return _(root)
+
+
+if __name__ == '__main__':
+    assert True == Solution().hasPathSum(
+        TreeNode(4,
+            TreeNode(2,
+                TreeNode(1),
+                TreeNode(3)
+             ),
+            TreeNode(7,
+                TreeNode(6),
+                TreeNode(9)
+             )
+        ),
+        targetSum=9
+    )
