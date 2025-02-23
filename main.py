8958d21ec818cb8f5b6aa8635fc6893d324809b2
Ilya (Marshal) <ilya@marshal.dev>
2025-02-23 16:23:53 +0100

    104. DFS

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def maxDepth(self, root: Optional[TreeNode], depth: int = 0) -> int:
         if not root:
-            return root
+            return 0
 
-        root.left, root.right = root.right, root.left
-
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-
-        return root
-
-
-if __name__ == '__main__':
-    Solution().invertTree(
-        TreeNode(4,
-            TreeNode(2,
-                TreeNode(1),
-                TreeNode(3)
-             ),
-            TreeNode(7,
-                TreeNode(6),
-                TreeNode(9)
-             )
-        )
-    )
+        return max(self.maxDepth(root.left, depth + 1), self.maxDepth(root.right, depth + 1))
