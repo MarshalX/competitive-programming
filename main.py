a5f2615bbb1532e16cc21ff4e4c6d457944d81cc
Ilya (Marshal) <ilya@marshal.dev>
2025-02-27 20:21:46 +0100

    155

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from typing import List
+class MinStack:
+    def __init__(self):
+        self._stack = []
+        self._min = [float('inf')] * (10 ** 4)
 
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+        self._min[len(self._stack)] = min(val, self._min[len(self._stack) - 1])
 
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack, res = [], [0] * len(temperatures)
+    def pop(self) -> None:
+        self._stack.pop()
+        self._min[len(self._stack) + 1] = float('inf')
 
-        for i in range(len(temperatures)):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                j = stack.pop()
-                res[j] = i - j
+    def top(self) -> int:
+        return self._stack[-1]
 
-            stack.append(i)
-
-        return res
-
-
-if __name__ == '__main__':
-    assert [0] == Solution().dailyTemperatures([73])
-    assert [1, 0] == Solution().dailyTemperatures([73, 74])
-    assert [1,1,4,2,1,1,0,0] == Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
-    assert [1,1,1,0] == Solution().dailyTemperatures([30,40,50,60])
+    def getMin(self) -> int:
+        return self._min[len(self._stack)]
