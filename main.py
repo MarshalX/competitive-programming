def48f16ccb8744556622daceaa574449b357c2b
Ilya (Marshal) <ilya@marshal.dev>
2025-03-04 23:34:39 +0100

    101. BFS

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 from typing import Optional
 
 
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
-
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
 
-        cloned_root_node = Node(node.val)
-        created_nodes = {node.val: cloned_root_node}
-
-        queue = [(cloned_root_node, node)]
-        visited = {node.val,}
+        queue = [(root.left, root.right)]
         while queue:
-            cloned_node, real_node = queue.pop(0)
-            for neighbor in real_node.neighbors:
-                if neighbor.val in created_nodes:
-                    neighbor_cloned_node = created_nodes[neighbor.val]
-                else:
-                    neighbor_cloned_node = Node(neighbor.val)
-                    created_nodes[neighbor.val] = neighbor_cloned_node
+            left, right = queue.pop(0)
+            if not left and not right:
+                continue
 
-                cloned_node.neighbors.append(neighbor_cloned_node)
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
 
-                if neighbor.val not in visited:
-                    queue.append((neighbor_cloned_node, neighbor))
-                    visited.add(neighbor.val)
+            queue.append((left.left, right.right))
+            queue.append((left.right, right.left))
 
-        return cloned_root_node
+        return True
