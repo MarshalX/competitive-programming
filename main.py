a1a28600b6960d90fbc3929c7aa43f48853b15e1
Ilya (Marshal) <ilya@marshal.dev>
2026-01-07 22:52:47 +0100

    3798

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-from typing import Optional
-
-
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-
-        queue = [(root.left, root.right)]
-        while queue:
-            left, right = queue.pop(0)
-            if not left and not right:
-                continue
-
-            if not left or not right:
-                return False
-            if left.val != right.val:
-                return False
-
-            queue.append((left.left, right.right))
-            queue.append((left.right, right.left))
-
-        return True
+    def largestEven(self, s: str) -> str:
+        return s.rstrip('1')
