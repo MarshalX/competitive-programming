2ea814c2aa67c92be3dd7b4151e6440b275d7716
Ilya (Marshal) <ilya@marshal.dev>
2026-01-07 23:37:35 +0100

    3769. Less symbols

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from typing import List
 
 class Solution:
     def sortByReflection(self, nums: List[int]) -> List[int]:
-        return [n for _, n in sorted([(int(bin(n)[2::][::-1], 2), n) for n in sorted(nums)], key=lambda n: n[0])]
+        return sorted(nums, key=lambda n: (int(bin(n)[2:][::-1]), n))
