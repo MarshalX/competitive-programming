ebac8b45435d4836cff3e52a2ac22c969ef333ad
Ilya (Marshal) <ilya@marshal.dev>
2026-01-07 23:08:15 +0100

    3774

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from typing import List
+
+
 class Solution:
-    def mirrorDistance(self, n: int) -> int:
-        return abs(n - int(str(n)[::-1]))
+    def absDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        s = sum(nums[:k])
+        l = sum(nums[-k:])
+        return abs(s - l)
