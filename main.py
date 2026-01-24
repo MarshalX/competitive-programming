99dc0452ded71de72e5916e38079d2fd3d248e22
Ilya (Marshal) <ilya@marshal.dev>
2026-01-24 18:43:14 +0100

    783

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from typing import List
+from typing import Optional
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def sortByReflection(self, nums: List[int]) -> List[int]:
-        return sorted(nums, key=lambda n: (int(bin(n)[2:][::-1]), n))
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        self.prev_val, self.result = None, float("inf")
+
+        def dfs(node):
+            if node is None:
+                return
+
+            dfs(node.left)
+
+            if self.prev_val is not None:
+                self.result = min(self.result, node.val - self.prev_val)
+
+            self.prev_val = node.val
+
+            dfs(node.right)
+
+        dfs(root)
+        return self.result
